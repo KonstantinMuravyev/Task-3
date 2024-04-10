c44b1a5df0dfe93de10b1703a09927d0fee86ff5
KonstantinMuravyev <k.muravyev2016@yandex.ru>
2024-04-11 04:59:35 +1000

    add App

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,13 @@
 package app.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
-    private Long nanoTime = System.nanoTime();
+    // System.nanoTime(); возвращает текущее время в наносекундах, возвращает более точный результат,
+    // чем метод System.currentTimeMillis(), который возварщает время в миллисекундах.
+    private final Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
